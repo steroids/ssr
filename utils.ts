a1c8b74947e89fd538a2fef8b632c4727cc83673
antonovaekaterina <antonova.ekaterina19@yandex.ru>
2021-05-27 17:14:41 +0700

    ssr init

---
+++ utils.ts
@@ -0,0 +1,26 @@
+interface IAsset {
+    name: string,
+    size: number,
+    chunks: string[],
+    chunkNames: string[],
+    info: Record<string, any>,
+    emitted: boolean
+}
+
+interface IStats {
+    errors: any[],
+    warnings: any[],
+    assetsByChunkName: Record<string, any>,
+    assets: IAsset[],
+    filteredAssets: number,
+    assetsUrls: string[]
+}
+
+export const getAssets = (stats: IStats): Record<string, string[]> => {
+    const assets = stats.assets.filter(asset => asset.chunks.includes('index') || asset.chunks.includes('common'));
+
+    return {
+        css: assets.filter(asset => /\.css/.test(asset.name)).map(asset => asset.name),
+        js: assets.filter(asset => /\.js/.test(asset.name)).map(asset => asset.name),
+    }
+}
