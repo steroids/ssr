0114a170eb9fd8c4cebb1dfc7979f33c6323205b
Marina Shakarova <marina.seed25@gmail.com>
2024-03-15 13:52:18 +0700

    upgrade webpack form v4 to v5: add todo to remove asset.chunks in getAssets function

--- utils/getAssets.ts
+++ utils/getAssets.ts
@@ -18,6 +18,9 @@ interface IStats {
 
 const getAssets = (stats: IStats): Record<string, string[]> => {
     const assets = stats.assets.filter(asset => {
+        // TODO: remove asset.chunks after transition from Webpack 4 to Webpack 5.
+        //  asset.chunks are using with Webpack 4, in Webpack 5 they don't exist.
+        //  asset.chunkNames are using with Webpack 5 instead of assets.chunks
         const chunkHints = asset.chunks || asset.chunkNames;
 
         return chunkHints.includes('index') || chunkHints.includes('common')
